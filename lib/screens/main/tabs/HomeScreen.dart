0645b4f83b158dd8a0392f1773139996458a1b7f
Gabriel Vargas <gabrielfvf@outlook.com>
2020-08-03 22:22:35 -0300

    Mais ajustes na home

--- lib/screens/main/tabs/HomeScreen.dart
+++ lib/screens/main/tabs/HomeScreen.dart
@@ -33,17 +33,161 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Stack(
-                    children: <Widget>[
-                      Text(
-                        'Olá, ${widget._username}!\nBateu a fome?',
-                        textAlign: TextAlign.start,
-                        style: Theme.of(context).textTheme.headline1,
-                      ),
-                    ],
+                  Text(
+                    'Olá, ${widget._username}!\nBateu a fome?',
+                    textAlign: TextAlign.start,
+                    style: Theme.of(context).textTheme.headline1,
                   ),
-                  SizedBox(height: 10),
-                  Text('Sugestões do dia'),
+                  SizedBox(height: 50),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Sugestões do dia',
+                          style: TextStyle(
+                            color: Theme.of(context).accentColor,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Container(
+                          height: 120,
+                          width: double.infinity,
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            children: <Widget>[
+                              Card(
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)),
+                                child: Column(
+                                  children: <Widget>[
+                                    Image.network(
+                                      'https://img.itdg.com.br/tdg/images/recipes/000/174/031/173715/173715_original.jpg?mode=crop&width=710&height=400',
+                                      width: 150,
+                                    ),
+                                    Text("Strogonoff"),
+                                  ],
+                                ),
+                              ),
+                              Card(
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)),
+                                child: Column(
+                                  children: <Widget>[
+                                    Image.network(
+                                      'https://img.itdg.com.br/tdg/images/recipes/000/174/031/173715/173715_original.jpg?mode=crop&width=710&height=400',
+                                      width: 150,
+                                    ),
+                                    Text("Teste"),
+                                  ],
+                                ),
+                              ),
+                              Card(
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(8)),
+                                child: Column(
+                                  children: <Widget>[
+                                    Image.network(
+                                      'https://img.itdg.com.br/tdg/images/recipes/000/174/031/173715/173715_original.jpg?mode=crop&width=710&height=400',
+                                      width: 150,
+                                    ),
+                                    Text("Teste2"),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Promoções da semana',
+                          style: TextStyle(
+                            color: Theme.of(context).accentColor,
+                            fontSize: 16,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Container(
+                            height: 220,
+                            child: GridView.count(
+                              childAspectRatio: (150 / 100),
+                              crossAxisCount: 2,
+                              children: <Widget>[
+                                Container(
+                                  child: Card(
+                                    clipBehavior: Clip.antiAliasWithSaveLayer,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(8)),
+                                    child: Column(
+                                      children: <Widget>[
+                                        Image.network(
+                                          'https://img.itdg.com.br/tdg/images/recipes/000/174/031/173715/173715_original.jpg?mode=crop&width=710&height=400',
+                                          width: 150,
+                                        ),
+                                        Text("Strogonoff"),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Card(
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8)),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Image.network(
+                                        'https://img.itdg.com.br/tdg/images/recipes/000/174/031/173715/173715_original.jpg?mode=crop&width=710&height=400',
+                                        width: 150,
+                                      ),
+                                      Text("Strogonoff"),
+                                    ],
+                                  ),
+                                ),
+                                Card(
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8)),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Image.network(
+                                        'https://img.itdg.com.br/tdg/images/recipes/000/174/031/173715/173715_original.jpg?mode=crop&width=710&height=400',
+                                        width: 150,
+                                      ),
+                                      Text("Strogonoff"),
+                                    ],
+                                  ),
+                                ),
+                                Card(
+                                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8)),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Image.network(
+                                        'https://img.itdg.com.br/tdg/images/recipes/000/174/031/173715/173715_original.jpg?mode=crop&width=710&height=400',
+                                        width: 150,
+                                      ),
+                                      Text("Strogonoff"),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ))
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
