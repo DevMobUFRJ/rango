6c22fd5c45cd45393aca5e0aa6094009209e74ed
Gabriel Vargas <gabrielfvf@outlook.com>
2020-08-12 15:53:28 -0300

    Ajuste de layout na tela de sobre

--- lib/screens/main/profile/AboutScreen.dart
+++ lib/screens/main/profile/AboutScreen.dart
@@ -27,43 +27,48 @@ class AboutScreen extends StatelessWidget {
                 maxHeight: MediaQuery.of(context).size.height * 0.9,
               ),
               child: Padding(
-                padding: EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    Text(
-                      'Rango é um aplicativo desenvolvido por pessoas que anseiam pelo momento do almoço, e o entendem como uma brecha de prazer e distração em meio a um dia corrido. \nFeito para ajudar o rango universitário a acontecer de forma mais organizada, nesse aplicativo conectamos quem quer comprar a quem quer vender, de modo que a comunicação seja mais clara e concisa. Assim, facilitamos o horário de almoço para todo mundo e ninguém passa fome!',
-                      textAlign: TextAlign.left,
-                      style: GoogleFonts.montserrat(
-                        fontSize: 16,
-                        color: Colors.grey[600],
+                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 25),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.9,
+                  child: Column(
+                    children: [
+                      Text(
+                        'Rango é um aplicativo desenvolvido por pessoas que anseiam pelo momento do almoço, e o entendem como uma brecha de prazer e distração em meio a um dia corrido. \n\nFeito para ajudar o rango universitário a acontecer de forma mais organizada, nesse aplicativo conectamos quem quer comprar a quem quer vender, de modo que a comunicação seja mais clara e concisa. Assim, facilitamos o horário de almoço para todo mundo e ninguém passa fome!',
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.montserrat(
+                          fontSize: 16,
+                          color: Colors.grey[600],
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'Protótipo feito por: Fernanda Arnaut, Júlia Lopes e Luísa Forain.',
-                      textAlign: TextAlign.left,
-                      style: GoogleFonts.montserrat(
-                        fontSize: 16,
-                        color: Colors.grey[600],
+                      SizedBox(height: 10),
+                      Text(
+                        'Protótipo feito por: Fernanda Arnaut, Júlia Lopes e Luísa Forain.',
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.montserrat(
+                          fontSize: 16,
+                          color: Colors.grey[600],
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      'Aplicativo desenvolvido como TCC por Gabriel Vargas, Guilherme Franco e Rebeca Fonseca.',
-                      textAlign: TextAlign.left,
-                      style: GoogleFonts.montserrat(
-                        fontSize: 16,
-                        color: Colors.grey[600],
+                      SizedBox(height: 10),
+                      Text(
+                        'Aplicativo desenvolvido como TCC por Gabriel Vargas, Guilherme Franco e Rebeca Fonseca.',
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.montserrat(
+                          fontSize: 16,
+                          color: Colors.grey[600],
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20),
-                    Container(
-                      child: Image.asset(
-                        'assets/imgs/logo_devmob.png',
-                        width: 150,
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.08),
+                      Container(
+                        alignment: Alignment.bottomCenter,
+                        child: Image.asset(
+                          'assets/imgs/logo_devmob.png',
+                          width: 125,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
