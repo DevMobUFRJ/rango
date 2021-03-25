de57b486b6b67c597e4a3e680f703c99a238ed47
Gabriel Vargas <gabrielfvf@outlook.com>
2021-03-25 16:58:47 -0300

    Adicionado telefones aos clientes e fotos às marmitas nos dadosMarretado

--- lib/dadosMarretados.dart
+++ lib/dadosMarretados.dart
@@ -3,9 +3,10 @@ import 'package:rango/models/meals.dart';
 import 'package:rango/models/order.dart';
 
 final List<Client> clients = [
-  Client(email: 'gabriel@email.com', name: 'Gabriel'),
-  Client(email: "guilherme@email.com", name: 'Guilherme'),
-  Client(email: 'rebeca@email.com', name: 'Rebeca')
+  Client(email: 'gabriel@email.com', name: 'Gabriel', phone: "(21)123456789"),
+  Client(
+      email: "guilherme@email.com", name: 'Guilherme', phone: "(21)123456789"),
+  Client(email: 'rebeca@email.com', name: 'Rebeca', phone: "(21)123456789")
 ];
 
 final List<Meal> quentinhas = [
@@ -14,11 +15,15 @@ final List<Meal> quentinhas = [
       description: '',
       price: 10,
       sellerId: '1',
+      picture:
+          "https://conteudo.imguol.com.br/c/entretenimento/02/2020/03/31/moqueca-de-peixe-1585666205541_v2_900x506.jpg.webp",
       sellerName: 'Quentinha Top'),
   Meal(
       name: "Lasanha",
       description: '',
       price: 10,
+      picture:
+          "https://img.itdg.com.br/tdg/images/recipes/000/000/876/324587/324587_original.jpg?mode=crop&width=710&height=400",
       sellerId: '1',
       sellerName: 'Quentinha Top'),
 ];
