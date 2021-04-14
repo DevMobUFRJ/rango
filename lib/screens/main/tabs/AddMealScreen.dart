42fb666289a0e70835d5785e77129f61fdb5aa06
Gabriel Vargas <gabrielfvf@outlook.com>
2021-04-13 21:54:52 -0300

    WIP: Tela de adicionar quentinhas

--- lib/screens/main/tabs/AddMealScreen.dart
+++ lib/screens/main/tabs/AddMealScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
@@ -8,6 +10,8 @@ import 'package:rango/dadosMarretados.dart';
 import 'package:rango/models/seller.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:rango/screens/seller/ManageOrder.dart';
+import 'package:rango/widgets/home/GridHorizontal.dart';
+import 'package:rango/widgets/quentinha/ListaHorizontal.dart';
 
 class AddMealScreen extends StatefulWidget {
   final Seller usuario;
@@ -21,12 +25,14 @@ class AddMealScreen extends StatefulWidget {
 class _AddMealScreenState extends State<AddMealScreen> {
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context, width: 750, height: 1334);
     final String assetName = 'assets/imgs/curva_principal.svg';
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
       body: Container(
-        constraints: BoxConstraints(maxWidth: 1.wp, maxHeight: 1.hp),
+        height: 1.hp - 56,
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             Stack(
               children: [
@@ -53,45 +59,38 @@ class _AddMealScreenState extends State<AddMealScreen> {
                 ),
               ],
             ),
-            SizedBox(
-              height: 0.04.hp,
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 0.03.wp),
+                child: AutoSizeText(
+                  "Cardápio do dia",
+                  style: GoogleFonts.montserrat(
+                    color: Theme.of(context).accentColor,
+                    fontSize: 28.ssp,
+                  ),
+                ),
+              ),
             ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 0.04.wp),
-              alignment: Alignment.centerLeft,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  AutoSizeText(
-                    "Cardápio do dia",
-                    textAlign: TextAlign.start,
-                    style: GoogleFonts.montserrat(
-                      color: Theme.of(context).accentColor,
-                      fontSize: 28.ssp,
-                    ),
+            GridHorizontal(
+              tagM: Random().nextDouble(),
+              orders: pedidos,
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                  SizedBox(height: 0.01.hp),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      width: 0.8.wp,
-                      child: RaisedButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        padding: EdgeInsets.symmetric(horizontal: 0.1.wp),
-                        onPressed: () =>
-                            pushNewScreen(context, screen: ManageOrder()),
-                        child: AutoSizeText("Adicionar prato"),
-                      ),
-                    ),
-                  ),
-                ],
+                  onPressed: () =>
+                      pushNewScreen(context, screen: ManageOrder()),
+                  child: AutoSizeText("Adicionar prato"),
+                ),
               ),
             ),
             SizedBox(
-              height: 0.02.hp,
+              height: 0.01.hp,
             ),
             Container(
               margin:
@@ -109,31 +108,34 @@ class _AddMealScreenState extends State<AddMealScreen> {
             ),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 0.2.wp),
-              child: ListView.builder(
-                padding: EdgeInsets.all(0),
-                itemCount: pedidos.length,
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemBuilder: (ctx, index) => Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      constraints: BoxConstraints(maxWidth: 0.5.wp),
-                      child: AutoSizeText(
-                        pedidos[index].quentinha.name,
-                        overflow: TextOverflow.ellipsis,
-                        style: GoogleFonts.montserrat(
-                          color: Color(0xFFF9B152),
-                          fontWeight: FontWeight.w500,
-                          fontSize: 28.ssp,
+              child: Container(
+                constraints: BoxConstraints(maxHeight: 0.25.hp),
+                child: ListView.builder(
+                  padding: EdgeInsets.all(0),
+                  itemCount: pedidos.length,
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemBuilder: (ctx, index) => Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        constraints: BoxConstraints(maxWidth: 0.5.wp),
+                        child: AutoSizeText(
+                          pedidos[index].quentinha.name,
+                          overflow: TextOverflow.ellipsis,
+                          style: GoogleFonts.montserrat(
+                            color: Color(0xFFF9B152),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 28.ssp,
+                          ),
                         ),
                       ),
-                    ),
-                    Icon(
-                      Icons.add_circle,
-                      color: Color(0xFFF9B152),
-                    ),
-                  ],
+                      Icon(
+                        Icons.add_circle,
+                        color: Color(0xFFF9B152),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
---
+++ lib/widgets/home/GridHorizontal.dart
@@ -0,0 +1,88 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+import 'package:flutter/foundation.dart';
+import 'package:rango/models/order.dart';
+import 'package:rango/widgets/quentinha/DetalhesQuentinhaScreen.dart';
+
+class GridHorizontal extends StatelessWidget {
+  final double tagM;
+  final List<Order> orders;
+
+  GridHorizontal({
+    @required this.tagM,
+    @required this.orders,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          height: 0.33.hp,
+          width: double.infinity,
+          child: StaggeredGridView.countBuilder(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            crossAxisCount: orders.length > 6 ? 2 : 1,
+            mainAxisSpacing: 0,
+            crossAxisSpacing: 0,
+            padding: EdgeInsets.all(0),
+            itemCount: orders.length,
+            itemBuilder: (ctx, index) => GestureDetector(
+              onTap: () => pushNewScreen(context,
+                  screen: DetalhesQuentinhaScreen(
+                      marmita: orders[index].quentinha, tagM: tagM),
+                  withNavBar: true,
+                  pageTransitionAnimation: PageTransitionAnimation.cupertino),
+              child: Container(
+                child: Card(
+                  semanticContainer: true,
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Column(
+                    children: <Widget>[
+                      Hero(
+                        tag: orders[index].quentinha.hashCode * tagM,
+                        child: FadeInImage.assetNetwork(
+                          placeholder: 'assets/imgs/quentinha_placeholder.png',
+                          image: orders[index].quentinha.picture,
+                          fit: BoxFit.fitWidth,
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 5),
+                        child: Column(
+                          children: <Widget>[
+                            AutoSizeText(
+                              orders[index].quentinha.name,
+                              textAlign: TextAlign.center,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: GoogleFonts.montserrat(fontSize: 24.nsp),
+                            ),
+                            AutoSizeText(
+                              'R\$${orders[index].quentinha.price}',
+                              style: GoogleFonts.montserrat(fontSize: 24.nsp),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+          ),
+        ),
+      ],
+    );
+  }
+}
