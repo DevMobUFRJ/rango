41ddfbdd2b79bd0e827c0605cabe6561f7218114
Guilherme Franco <guilherme.franco@moneri.com.br>
2021-04-14 17:30:09 -0300

    Query sellers por localização

--- lib/screens/main/tabs/HomeScreen.dart
+++ lib/screens/main/tabs/HomeScreen.dart
@@ -4,13 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:rango/dadosMarretados.dart';
 import 'package:rango/models/client.dart';
 import 'package:rango/models/meals.dart';
 import 'package:rango/widgets/home/GridVertical.dart';
 import 'package:rango/widgets/home/ListaHorizontal.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart:convert';
+import 'package:geoflutterfire/geoflutterfire.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomeScreen extends StatefulWidget {
   final Client usuario;
@@ -24,31 +24,45 @@ class HomeScreen extends StatefulWidget {
 List<Meal> meals = [];
 
 class _HomeScreenState extends State<HomeScreen> {
+  final geo = Geoflutterfire();
   final _database = Firestore.instance;
   void _fetch() async {
+    var userLocation = await Geolocator.getCurrentPosition();
+    print("User location: $userLocation");
+
+    GeoFirePoint center = geo.point(latitude: userLocation.latitude, longitude: userLocation.longitude); // Niterói
+    double radius = 30; // Em km
+
     try {
       print("Start fetch");
-      await _database.collection("sellers")
-          .getDocuments()
-          .then((QuerySnapshot snapshot) {
-            List<Meal> tempMeals = [];
-            print(snapshot.documents.length.toString() + " sellers found");
-            for(var i=0; i<snapshot.documents.length; i++){
-              // Iterar todos os vendedores
-              var seller = snapshot.documents[i];
-              seller.reference.collection("meals")
+      Stream<List<DocumentSnapshot>> stream = geo.collection(collectionRef: _database.collection("sellers"))
+          .within(center: center, radius: radius, field: "location", strictMode: true);
+
+      stream.listen((List<DocumentSnapshot> documentList) {
+        List<Meal> tempMeals = [];
+        print("${documentList.length} sellers found");
+
+        for(var i=0; i < documentList.length; i++){
+          // Iterar todos os vendedores
+          var seller = documentList[i];
+          print("Distance to seller ${seller.data["name"]}: ${center.distance(lat: seller.data["location"]["geopoint"].latitude, lng: seller.data["location"]["geopoint"].longitude)} km");
+
+          // TODO Limitar quantidade de pratos para seção Sugestões ou filtrar por featured?
+          seller.reference.collection("meals")
               .getDocuments()
               .then((QuerySnapshot snapshot) {
-                snapshot.documents.forEach((meal) {
-                  // Iterar todos os meals
-                  Meal currentMeal = Meal.fromJson(meal.data);
-                  currentMeal.sellerName = seller.data["name"];
-                  currentMeal.sellerId = seller.documentID;
-                  tempMeals.add(currentMeal);
-                });
-              });
-            }
-            meals = tempMeals;
+            snapshot.documents.forEach((meal) {
+              // Iterar todos os meals
+              // print(meal.reference.parent().parent().documentID);
+              Meal currentMeal = Meal.fromJson(meal.data);
+              currentMeal.sellerName = seller.data["name"];
+              currentMeal.sellerId = seller.documentID;
+              tempMeals.add(currentMeal);
+            });
+          });
+        }
+        tempMeals.shuffle();
+        meals = tempMeals;
       });
     } catch(e) {
       print(e);
@@ -59,7 +73,6 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     ScreenUtil.init(context, width: 750, height: 1334);
     _fetch();
-    print(meals);
     final String assetName = 'assets/imgs/curva_principal.svg';
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
