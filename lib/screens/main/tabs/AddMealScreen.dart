ad308e72ef7c68a2d6b0c1baf3caaf749623ad16
Gabriel Vargas <gabrielfvf@outlook.com>
2021-05-08 19:59:26 -0300

    Ajustes no layout da tab de add pedidos

--- lib/screens/main/tabs/AddMealScreen.dart
+++ lib/screens/main/tabs/AddMealScreen.dart
@@ -56,114 +56,140 @@ class _AddMealScreenState extends State<AddMealScreen> {
                 ),
               ],
             ),
-            Align(
-              alignment: Alignment.topLeft,
+            Flexible(
+              flex: 4,
               child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 0.03.wp),
-                child: AutoSizeText(
-                  "Cardápio do dia",
-                  style: GoogleFonts.montserrat(
-                    color: Theme.of(context).accentColor,
-                    fontSize: 28.ssp,
-                  ),
-                ),
-              ),
-            ),
-            GridHorizontal(
-              orders: pedidos,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    margin: EdgeInsets.only(right: 0.05.wp),
-                    child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      onPressed: () =>
-                          pushNewScreen(context, screen: ManageOrder()),
-                      child: AutoSizeText(
-                        "Adicionar",
-                        style: GoogleFonts.montserrat(
-                          fontSize: 32.nsp,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: Container(
-                    child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      onPressed: () => pushNewScreen(
-                        context,
-                        screen: MealsHistory(orders: pedidos),
-                      ),
-                      child: AutoSizeText(
-                        "Histórico",
-                        style: GoogleFonts.montserrat(
-                          fontSize: 32.nsp,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 0.01.hp,
-            ),
-            Container(
-              margin:
-                  EdgeInsets.symmetric(horizontal: 0.04.wp, vertical: 0.01.hp),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: AutoSizeText(
-                  "Adicionar pratos já cadastrados:",
-                  style: GoogleFonts.montserrat(
-                    color: Theme.of(context).accentColor,
-                    fontSize: 28.ssp,
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 0.2.wp),
-              child: Container(
-                constraints: BoxConstraints(maxHeight: 0.25.hp),
-                child: ListView.builder(
-                  padding: EdgeInsets.all(0),
-                  itemCount: pedidos.length,
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  itemBuilder: (ctx, index) => Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        constraints: BoxConstraints(maxWidth: 0.5.wp),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Container(
+                        margin: EdgeInsets.symmetric(horizontal: 0.03.wp),
                         child: AutoSizeText(
-                          pedidos[index].quentinha.name,
-                          overflow: TextOverflow.ellipsis,
+                          "Cardápio do dia",
                           style: GoogleFonts.montserrat(
-                            color: Color(0xFFF9B152),
-                            fontWeight: FontWeight.w500,
-                            fontSize: 28.ssp,
+                            color: Theme.of(context).accentColor,
+                            fontSize: 30.ssp,
                           ),
                         ),
                       ),
-                      Icon(
-                        Icons.add_circle,
-                        color: Color(0xFFF9B152),
+                    ),
+                    Flexible(
+                      flex: 4,
+                      child: Container(
+                        child: GridHorizontal(
+                          orders: pedidos,
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Flexible(
+                        flex: 2,
+                        child: Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Align(
+                                alignment: Alignment.center,
+                                child: Container(
+                                  margin: EdgeInsets.only(right: 0.05.wp),
+                                  child: RaisedButton(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    onPressed: () => pushNewScreen(context,
+                                        screen: ManageOrder()),
+                                    child: AutoSizeText(
+                                      "Adicionar",
+                                      style: GoogleFonts.montserrat(
+                                        fontSize: 32.nsp,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.center,
+                                child: Container(
+                                  child: RaisedButton(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    onPressed: () => pushNewScreen(
+                                      context,
+                                      screen: MealsHistory(orders: pedidos),
+                                    ),
+                                    child: AutoSizeText(
+                                      "Histórico",
+                                      style: GoogleFonts.montserrat(
+                                        fontSize: 32.nsp,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )),
+                  ],
                 ),
+              ),
+            ),
+            Flexible(
+              flex: 3,
+              child: Column(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: AutoSizeText(
+                        "Adicionar pratos já cadastrados:",
+                        style: GoogleFonts.montserrat(
+                          color: Theme.of(context).accentColor,
+                          fontSize: 32.nsp,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 9,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 0.2.wp),
+                      child: Container(
+                        child: ListView.builder(
+                          padding: EdgeInsets.all(0),
+                          itemCount: pedidos.length,
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemBuilder: (ctx, index) => Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                constraints: BoxConstraints(maxWidth: 0.5.wp),
+                                child: GestureDetector(
+                                  onTap: () => {},
+                                  child: AutoSizeText(
+                                    pedidos[index].quentinha.name,
+                                    overflow: TextOverflow.ellipsis,
+                                    style: GoogleFonts.montserrat(
+                                      color: Color(0xFFF9B152),
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 28.ssp,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Icon(
+                                Icons.add_circle,
+                                color: Color(0xFFF9B152),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
--- lib/widgets/home/GridHorizontal.dart
+++ lib/widgets/home/GridHorizontal.dart
@@ -17,68 +17,58 @@ class GridHorizontal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Container(
-          height: 0.33.hp,
-          width: double.infinity,
-          child: StaggeredGridView.countBuilder(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            crossAxisCount: orders.length > 6 ? 2 : 1,
-            mainAxisSpacing: 0,
-            crossAxisSpacing: 0,
-            padding: EdgeInsets.all(0),
-            itemCount: orders.length,
-            itemBuilder: (ctx, index) => GestureDetector(
-              onTap: () => pushNewScreen(context,
-                  screen: ManageOrder(
-                    order: orders[index],
-                  ),
-                  withNavBar: true,
-                  pageTransitionAnimation: PageTransitionAnimation.cupertino),
-              child: Container(
-                child: Card(
-                  semanticContainer: true,
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)),
-                  child: Column(
-                    children: <Widget>[
-                      FadeInImage.assetNetwork(
-                        placeholder: 'assets/imgs/quentinha_placeholder.png',
-                        image: orders[index].quentinha.picture,
-                        fit: BoxFit.fitWidth,
+    return StaggeredGridView.countBuilder(
+      scrollDirection: Axis.horizontal,
+      crossAxisCount: orders.length > 6 ? 2 : 1,
+      mainAxisSpacing: 0,
+      crossAxisSpacing: 0,
+      padding: EdgeInsets.all(0),
+      itemCount: orders.length,
+      itemBuilder: (ctx, index) => GestureDetector(
+        onTap: () => pushNewScreen(context,
+            screen: ManageOrder(
+              order: orders[index],
+            ),
+            withNavBar: true,
+            pageTransitionAnimation: PageTransitionAnimation.cupertino),
+        child: Card(
+          semanticContainer: true,
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          child: Column(
+            children: <Widget>[
+              FadeInImage.assetNetwork(
+                placeholder: 'assets/imgs/quentinha_placeholder.png',
+                image: orders[index].quentinha.picture,
+                fit: BoxFit.fitWidth,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                child: Column(
+                  children: <Widget>[
+                    AutoSizeText(
+                      orders[index].quentinha.name,
+                      textAlign: TextAlign.center,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: GoogleFonts.montserrat(
+                        fontSize: orders.length > 6 ? 24.nsp : 28.nsp,
                       ),
-                      Container(
-                        padding: EdgeInsets.symmetric(vertical: 5),
-                        child: Column(
-                          children: <Widget>[
-                            AutoSizeText(
-                              orders[index].quentinha.name,
-                              textAlign: TextAlign.center,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: GoogleFonts.montserrat(fontSize: 24.nsp),
-                            ),
-                            AutoSizeText(
-                              'R\$${orders[index].quentinha.price}',
-                              style: GoogleFonts.montserrat(fontSize: 24.nsp),
-                            ),
-                          ],
-                        ),
+                    ),
+                    AutoSizeText(
+                      'R\$${orders[index].quentinha.price}',
+                      style: GoogleFonts.montserrat(
+                        fontSize: orders.length > 6 ? 24.nsp : 28.nsp,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+            ],
           ),
         ),
-      ],
+      ),
+      staggeredTileBuilder: (index) => StaggeredTile.fit(1),
     );
   }
 }
