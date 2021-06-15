c8e2e7d7da37a03e3dd643030c8df8c25784de9a
Gabriel Vargas <gabrielfvf@outlook.com>
2021-06-14 23:11:50 -0300

    WIP: Copiar para área de transferência tel do cliente

--- lib/screens/main/home/ClientProfile.dart
+++ lib/screens/main/home/ClientProfile.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
@@ -48,75 +49,84 @@ class _ClientProfileState extends State<ClientProfile> {
       ),
       body: loading
           ? CircularProgressIndicator()
-          : Center(
-              child: Column(
-                children: [
-                  Container(
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.only(left: 80.w, bottom: 40.h),
-                          height: 210.h,
-                          width: 240.w,
-                          decoration: BoxDecoration(
-                            borderRadius:
-                                BorderRadius.circular(ScreenUtil().setSp(30)),
-                            color: yellow,
+          : Builder(
+              builder: (context) => Center(
+                child: Column(
+                  children: [
+                    Container(
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(left: 80.w, bottom: 40.h),
+                            height: 210.h,
+                            width: 240.w,
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.circular(ScreenUtil().setSp(30)),
+                              color: yellow,
+                            ),
                           ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 20),
-                          child: CircleAvatar(
-                            backgroundColor: Theme.of(context).accentColor,
-                            backgroundImage: client.picture != null
-                                ? NetworkImage(client.picture)
-                                : NetworkImage(
-                                    'https://ra.ac.ae/wp-content/uploads/2017/02/user-icon-placeholder.png'),
-                            radius: 130.w,
+                          Container(
+                            margin: EdgeInsets.only(top: 20),
+                            child: CircleAvatar(
+                              backgroundColor: Theme.of(context).accentColor,
+                              backgroundImage: client.picture != null
+                                  ? NetworkImage(client.picture)
+                                  : NetworkImage(
+                                      'https://ra.ac.ae/wp-content/uploads/2017/02/user-icon-placeholder.png'),
+                              radius: 130.w,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 20.h),
-                  if (client.phone != null)
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.phone, size: 38.nsp),
-                        Container(
-                          width: 170.w,
-                          child: AutoSizeText(
-                            client.phone,
-                            maxLines: 1,
-                          ),
-                        ),
-                      ],
-                    ),
-                  SizedBox(height: 40.h),
-                  RaisedButton.icon(
-                    icon: Icon(Icons.chat, size: 38.nsp),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    onPressed: () => pushNewScreen(
-                      context,
-                      screen: ChatScreen(client),
-                      withNavBar: false,
-                      pageTransitionAnimation:
-                          PageTransitionAnimation.cupertino,
-                    ),
-                    label: Container(
-                      width: 0.5.wp,
-                      child: AutoSizeText(
-                        'Chat com o cliente',
-                        maxLines: 1,
-                        style: GoogleFonts.montserrat(),
+                        ],
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 20.h),
+                    if (client.phone != null)
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.phone, size: 38.nsp),
+                          GestureDetector(
+                            onTap: () {
+                              //TODO clicar e copiar para área de transferência
+                            },
+                            child: Container(
+                              child: AutoSizeText(
+                                client.phone,
+                                maxLines: 1,
+                                style: GoogleFonts.montserrat(
+                                  fontSize: 30.nsp,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    SizedBox(height: 40.h),
+                    RaisedButton.icon(
+                      icon: Icon(Icons.chat, size: 38.nsp),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      onPressed: () => pushNewScreen(
+                        context,
+                        screen: ChatScreen(client),
+                        withNavBar: false,
+                        pageTransitionAnimation:
+                            PageTransitionAnimation.cupertino,
+                      ),
+                      label: Container(
+                        width: 0.5.wp,
+                        child: AutoSizeText(
+                          'Chat com o cliente',
+                          maxLines: 1,
+                          style: GoogleFonts.montserrat(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
     );
