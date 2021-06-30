4fa328d04ef7be9c0bcd05a0fd0bbb9a9ca716b9
Gabriel Vargas <gabrielfvf@outlook.com>
2021-06-30 15:32:12 -0300

    Ajuste de layout na tela de sobre

--- lib/screens/main/profile/AboutScreen.dart
+++ lib/screens/main/profile/AboutScreen.dart
@@ -6,7 +6,6 @@ import 'package:google_fonts/google_fonts.dart';
 class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init(context, width: 750, height: 1334);
     return Scaffold(
       appBar: AppBar(
         title: AutoSizeText(
@@ -18,86 +17,68 @@ class AboutScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: LayoutBuilder(
-        builder: (ctx, constraint) => SingleChildScrollView(
-          child: Container(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                maxHeight:
-                    MediaQuery.of(context).size.height - kToolbarHeight - 86,
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: 0.01.hp,
-                  horizontal: 0.08.wp,
-                ),
-                child: Container(
-                  height: 0.9.hp,
-                  child: Column(
-                    children: [
-                      Flexible(
-                        flex: 2,
-                        child: AutoSizeText(
-                          'Rango é um aplicativo desenvolvido por pessoas que anseiam pelo momento do almoço, e o entendem como uma brecha de prazer e distração em meio a um dia corrido.',
-                          textAlign: TextAlign.left,
-                          style: GoogleFonts.montserrat(
-                            color: Colors.grey[600],
-                            fontSize: 30.nsp,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 0.01.hp),
-                      Flexible(
-                        flex: 2,
-                        child: AutoSizeText(
-                          'Feito para ajudar o rango universitário a acontecer de forma mais organizada, nesse aplicativo conectamos quem quer comprar a quem quer vender, de modo que a comunicação seja mais clara e concisa. Assim, facilitamos o horário de almoço para todo mundo e ninguém passa fome!',
-                          textAlign: TextAlign.left,
-                          style: GoogleFonts.montserrat(
-                            color: Colors.grey[600],
-                            fontSize: 30.nsp,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 0.01.hp),
-                      Flexible(
-                        flex: 1,
-                        child: AutoSizeText(
-                          'Protótipo feito por: Fernanda Arnaut, Julia Lopes e Luísa Forain.',
-                          textAlign: TextAlign.left,
-                          style: GoogleFonts.montserrat(
-                            color: Colors.grey[600],
-                            fontSize: 30.nsp,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 0.01.hp),
-                      Flexible(
-                        flex: 1,
-                        child: AutoSizeText(
-                          'Aplicativo desenvolvido como TCC por Gabriel Vargas, Guilherme Franco e Rebeca Fonseca.',
-                          textAlign: TextAlign.left,
-                          style: GoogleFonts.montserrat(
-                            color: Colors.grey[600],
-                            fontSize: 30.nsp,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 0.01.hp),
-                      Expanded(
-                        flex: 3,
-                        child: Container(
-                          alignment: Alignment.bottomCenter,
-                          child: Image.asset(
-                            'assets/imgs/logo_devmob.png',
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Flexible(
+              flex: 2,
+              child: AutoSizeText(
+                'Rango é um aplicativo desenvolvido por pessoas que anseiam pelo momento do almoço, e o entendem como uma brecha de prazer e distração em meio a um dia corrido.',
+                textAlign: TextAlign.left,
+                style: GoogleFonts.montserrat(
+                  color: Colors.grey[600],
+                  fontSize: 30.nsp,
                 ),
               ),
             ),
-          ),
+            SizedBox(height: 0.01.hp),
+            Flexible(
+              flex: 3,
+              child: AutoSizeText(
+                'Feito para ajudar o rango universitário a acontecer de forma mais organizada, nesse aplicativo conectamos quem quer comprar a quem quer vender, de modo que a comunicação seja mais clara e concisa. Assim, facilitamos o horário de almoço para todo mundo e ninguém passa fome!',
+                textAlign: TextAlign.left,
+                style: GoogleFonts.montserrat(
+                  color: Colors.grey[600],
+                  fontSize: 35.nsp,
+                ),
+              ),
+            ),
+            SizedBox(height: 0.01.hp),
+            Flexible(
+              flex: 1,
+              child: AutoSizeText(
+                'Protótipo feito por: Fernanda Arnaut, Julia Lopes e Luísa Forain.',
+                textAlign: TextAlign.left,
+                style: GoogleFonts.montserrat(
+                  color: Colors.grey[600],
+                  fontSize: 30.nsp,
+                ),
+              ),
+            ),
+            SizedBox(height: 0.01.hp),
+            Flexible(
+              flex: 2,
+              child: AutoSizeText(
+                'Aplicativo desenvolvido como TCC por Gabriel Vargas, Guilherme Franco e Rebeca Fonseca.',
+                textAlign: TextAlign.left,
+                style: GoogleFonts.montserrat(
+                  color: Colors.grey[600],
+                  fontSize: 35.nsp,
+                ),
+              ),
+            ),
+            SizedBox(height: 0.01.hp),
+            Expanded(
+              flex: 4,
+              child: Container(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset(
+                  'assets/imgs/logo_devmob.png',
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
