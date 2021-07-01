3e30c9a87b22ee9802eaed55e41d2925eae52517
Gabriel Vargas <gabrielfvf@outlook.com>
2021-07-01 17:37:47 -0300

    Colocado slider de raio de busca de vendedores

--- lib/screens/main/profile/ProfileSettings.dart
+++ lib/screens/main/profile/ProfileSettings.dart
@@ -29,31 +29,32 @@ class _ProfileSettingsState extends State<ProfileSettings> {
   bool _reservaValue = false;
   bool _newMessagesValue = false;
   bool _promotionsValue = false;
-  TextEditingController _rangeController;
+  double _rangeValue;
 
   @override
   void initState() {
-    _rangeController =
-        TextEditingController(text: widget.sellerRange.toString());
+    _rangeValue = widget.sellerRange;
 
-    setState(() {
-      _attValue = widget.user.notificationSettings != null &&
-              widget.user.notificationSettings.favoriteSellers != null
-          ? widget.user.notificationSettings.favoriteSellers
-          : false;
-      _reservaValue = widget.user.notificationSettings != null &&
-              widget.user.notificationSettings.reservations != null
-          ? widget.user.notificationSettings.reservations
-          : false;
-      _newMessagesValue = widget.user.notificationSettings != null &&
-              widget.user.notificationSettings.messages != null
-          ? widget.user.notificationSettings.messages
-          : false;
-      _promotionsValue = widget.user.notificationSettings != null &&
-              widget.user.notificationSettings.discounts != null
-          ? widget.user.notificationSettings.discounts
-          : false;
-    });
+    setState(
+      () {
+        _attValue = widget.user.notificationSettings != null &&
+                widget.user.notificationSettings.favoriteSellers != null
+            ? widget.user.notificationSettings.favoriteSellers
+            : false;
+        _reservaValue = widget.user.notificationSettings != null &&
+                widget.user.notificationSettings.reservations != null
+            ? widget.user.notificationSettings.reservations
+            : false;
+        _newMessagesValue = widget.user.notificationSettings != null &&
+                widget.user.notificationSettings.messages != null
+            ? widget.user.notificationSettings.messages
+            : false;
+        _promotionsValue = widget.user.notificationSettings != null &&
+                widget.user.notificationSettings.discounts != null
+            ? widget.user.notificationSettings.discounts
+            : false;
+      },
+    );
     super.initState();
   }
 
@@ -125,190 +126,201 @@ class _ProfileSettingsState extends State<ProfileSettings> {
     const orange = Color(0xFFFC744F);
     const green = Color(0xFF609B90);
     return Scaffold(
-        appBar: AppBar(
-          title: AutoSizeText(
-            'Configurações',
-            style: GoogleFonts.montserrat(
-              color: Theme.of(context).accentColor,
-              fontSize: 38.nsp,
-            ),
+      appBar: AppBar(
+        title: AutoSizeText(
+          'Configurações',
+          style: GoogleFonts.montserrat(
+            color: Theme.of(context).accentColor,
+            fontSize: 38.nsp,
           ),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.only(top: 0.02.hp),
-            margin: EdgeInsets.symmetric(horizontal: 0.08.wp),
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.only(right: 0.09.wp),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      AutoSizeText(
-                        'Notificações',
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                          color: orange,
-                          fontSize: 38.nsp,
-                        ),
-                      ),
-                      Switch(
-                        value: _switchValue,
-                        activeColor: Color(0xFF609B90),
-                        onChanged: (value) => setState(() {
-                          _switchValue = value;
-                          if (value == false) {
-                            _attValue = false;
-                            _reservaValue = false;
-                            _newMessagesValue = false;
-                            _promotionsValue = false;
-                          }
-                        }),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.only(left: 0.02.wp),
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 0.07.hp,
-                        child: CustomCheckBox(
-                          changeValue: (value) =>
-                              setState(() => _attValue = value),
-                          text: 'Atualização dos vendedores favoritos',
-                          value: _attValue,
-                          isActive: _switchValue,
-                        ),
-                      ),
-                      Container(
-                        height: 0.07.hp,
-                        child: CustomCheckBox(
-                          changeValue: (value) =>
-                              setState(() => _reservaValue = value),
-                          text: 'Reserva confirmada',
-                          value: _reservaValue,
-                          isActive: _switchValue,
-                        ),
-                      ),
-                      Container(
-                        height: 0.07.hp,
-                        child: CustomCheckBox(
-                          changeValue: (value) =>
-                              setState(() => _newMessagesValue = value),
-                          text: 'Novas mensagens',
-                          value: _newMessagesValue,
-                          isActive: _switchValue,
-                        ),
-                      ),
-                      Container(
-                        height: 0.07.hp,
-                        child: CustomCheckBox(
-                          changeValue: (value) =>
-                              setState(() => _promotionsValue = value),
-                          text: 'Promoções',
-                          value: _promotionsValue,
-                          isActive: _switchValue,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 0.01.hp),
-                  width: 0.6.wp,
-                  // TODO Mudar isso para um slider?
-                  child: TextField(
-                    controller: _rangeController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      focusColor: orange,
-                      fillColor: orange,
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: orange),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: orange),
-                      ),
-                      labelStyle: GoogleFonts.montserrat(
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.only(top: 0.02.hp),
+          margin: EdgeInsets.symmetric(horizontal: 0.08.wp),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(right: 0.09.wp),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    AutoSizeText(
+                      'Notificações',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
                         color: orange,
-                        fontSize: 35.nsp,
+                        fontSize: 38.nsp,
                       ),
-                      labelText: 'Raio de busca (km)',
-                      hintText: 'Raio de busca',
-                      suffixText: 'km',
                     ),
-                  ),
+                    Switch(
+                      value: _switchValue,
+                      activeColor: Color(0xFF609B90),
+                      onChanged: (value) => setState(() {
+                        _switchValue = value;
+                        if (value == false) {
+                          _attValue = false;
+                          _reservaValue = false;
+                          _newMessagesValue = false;
+                          _promotionsValue = false;
+                        }
+                      }),
+                    ),
+                  ],
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 0.01.hp),
-                  width: 0.6.wp,
-                  child: ElevatedButton(
-                    onPressed: () => _saveSettings(context),
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                          vertical: 0.01.hp, horizontal: 0.1.wp),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 0.02.wp),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 0.07.hp,
+                      child: CustomCheckBox(
+                        changeValue: (value) =>
+                            setState(() => _attValue = value),
+                        text: 'Atualização dos vendedores favoritos',
+                        value: _attValue,
+                        isActive: _switchValue,
+                      ),
+                    ),
+                    Container(
+                      height: 0.07.hp,
+                      child: CustomCheckBox(
+                        changeValue: (value) =>
+                            setState(() => _reservaValue = value),
+                        text: 'Reserva confirmada',
+                        value: _reservaValue,
+                        isActive: _switchValue,
+                      ),
+                    ),
+                    Container(
+                      height: 0.07.hp,
+                      child: CustomCheckBox(
+                        changeValue: (value) =>
+                            setState(() => _newMessagesValue = value),
+                        text: 'Novas mensagens',
+                        value: _newMessagesValue,
+                        isActive: _switchValue,
+                      ),
+                    ),
+                    Container(
+                      height: 0.07.hp,
+                      child: CustomCheckBox(
+                        changeValue: (value) =>
+                            setState(() => _promotionsValue = value),
+                        text: 'Promoções',
+                        value: _promotionsValue,
+                        isActive: _switchValue,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 0.01.hp),
+                width: 0.75.wp,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AutoSizeText(
+                      'Raio de busca',
+                      style: GoogleFonts.montserrat(
+                        color: Theme.of(context).accentColor,
+                        fontSize: 30.nsp,
+                      ),
+                    ),
+                    Slider(
+                      value: _rangeValue,
+                      min: 0,
+                      max: 500,
+                      onChanged: (value) => setState(
+                        () => _rangeValue = value.round().toDouble(),
+                      ),
+                      activeColor: Theme.of(context).accentColor,
+                    ),
+                    Center(
                       child: AutoSizeText(
-                        'Confirmar',
+                        ' ${_rangeValue.ceil().toString()} Km',
                         style: GoogleFonts.montserrat(
-                          fontSize: 36.nsp,
+                          color: Theme.of(context).accentColor,
+                          fontSize: 30.nsp,
                         ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 0.02.hp),
-                  child: GestureDetector(
-                    onTap: () => pushNewScreen(
-                      context,
-                      screen: AboutScreen(),
-                      withNavBar: true,
-                      pageTransitionAnimation:
-                          PageTransitionAnimation.cupertino,
-                    ),
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: AutoSizeText(
-                        'Sobre',
-                        style: GoogleFonts.montserrat(
-                          fontSize: 36.nsp,
-                          decoration: TextDecoration.underline,
-                          color: green,
-                        ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 0.01.hp),
+                width: 0.6.wp,
+                child: ElevatedButton(
+                  onPressed: () => _saveSettings(context),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(
+                        vertical: 0.01.hp, horizontal: 0.1.wp),
+                    child: AutoSizeText(
+                      'Confirmar',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 36.nsp,
                       ),
                     ),
                   ),
                 ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 0.02.hp),
-                  child: GestureDetector(
-                    onTap: _logout,
-                    child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: AutoSizeText(
-                        'Sair',
-                        style: GoogleFonts.montserrat(
-                          fontSize: 36.nsp,
-                          decoration: TextDecoration.underline,
-                          color: green,
-                        ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 0.02.hp),
+                child: GestureDetector(
+                  onTap: () => pushNewScreen(
+                    context,
+                    screen: AboutScreen(),
+                    withNavBar: true,
+                    pageTransitionAnimation: PageTransitionAnimation.cupertino,
+                  ),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: AutoSizeText(
+                      'Sobre',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 36.nsp,
+                        decoration: TextDecoration.underline,
+                        color: green,
                       ),
                     ),
                   ),
-                )
-              ],
-            ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 0.02.hp),
+                child: GestureDetector(
+                  onTap: _logout,
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: AutoSizeText(
+                      'Sair',
+                      style: GoogleFonts.montserrat(
+                        fontSize: 36.nsp,
+                        decoration: TextDecoration.underline,
+                        color: green,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 
   void _saveSettings(context) async {
     try {
       await Repository.instance.setSellerRange(
-          double.parse(_rangeController.text.replaceAll(',', '.')));
+        double.parse(
+          _rangeValue.toString().replaceAll(',', '.'),
+        ),
+      );
       Provider.of<RangeChangeNotifier>(context, listen: false).triggerRefresh();
       FocusScope.of(context).unfocus();
       ScaffoldMessenger.of(context).showSnackBar(
