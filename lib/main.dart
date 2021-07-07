f7df5b9792f25daaa08f9addf014aa18b1ba418d
Gabriel Vargas <gabrielfvf@outlook.com>
2021-07-07 11:56:17 -0300

    Colocado Firebase Crashlytics no projeto

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp();
+  await Firebase.initializeApp();
   const AndroidInitializationSettings initializationAndroidSettings =
       AndroidInitializationSettings('app_icon');
   final InitializationSettings initializationSettings =
