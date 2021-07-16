8c406a02e0d640cec8303f4211fbae2810659486
Gabriel Vargas <gabrielfvf@outlook.com>
2021-07-16 18:13:04 -0300

    Colocado icone do app e loading na tela de esqueceu a senha

--- lib/screens/auth/ForgotPasswordScreen.dart
+++ lib/screens/auth/ForgotPasswordScreen.dart
@@ -12,12 +12,14 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  bool _loading = false;
   final _formKey = GlobalKey<FormState>();
   final _scaffoldKey = GlobalKey<ScaffoldState>();
   String _email;
   String _emailErrorMessage;
 
   Future<void> _submit(BuildContext ctx) async {
+    setState(() => _loading = true);
     final isValid = _formKey.currentState.validate();
     FocusScope.of(ctx).unfocus();
     if (isValid && _emailErrorMessage == null) {
@@ -25,6 +27,7 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
       try {
         final firebaseAuth = FirebaseAuth.instance;
         await firebaseAuth.sendPasswordResetEmail(email: _email);
+        setState(() => _loading = false);
         ScaffoldMessenger.of(context)
             .showSnackBar(
               SnackBar(
@@ -41,8 +44,25 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
               ),
             )
             .closed
-            .then((value) => Navigator.of(ctx).pop());
+            .then(
+              (value) => Navigator.of(ctx).pop(),
+            );
       } catch (error) {
+        setState(() => _loading = false);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Theme.of(ctx).accentColor,
+            duration: Duration(seconds: 2),
+            content: Text(
+              'Erro ao enviar email de recuperação',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.montserrat(
+                color: Colors.white,
+                fontSize: 28.nsp,
+              ),
+            ),
+          ),
+        );
         print(error);
       }
     }
@@ -50,7 +70,6 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init(context, width: 750, height: 1334);
     return Scaffold(
       key: _scaffoldKey,
       appBar: AppBar(
@@ -66,22 +85,33 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 0.1.wp),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Flexible(
               flex: 1,
-              child: Text(
-                "RANGO",
-                style: GoogleFonts.montserrat(
-                  fontSize: 80.nsp,
-                  color: Theme.of(context).accentColor,
-                ),
+              child: Column(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Image(image: AssetImage('assets/imgs/rango.png')),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Text(
+                      "RANGO",
+                      style: TextStyle(
+                        color: Theme.of(context).accentColor,
+                        fontSize: 80.nsp,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
             Flexible(
               flex: 1,
               child: AutoSizeText(
-                "Preencha o campo abaixo para receber o email de recuperação de senha",
+                "Preencha abaixo para receber o email de recuperação de senha",
                 style: GoogleFonts.montserrat(
                   fontSize: 30.nsp,
                   color: Theme.of(context).accentColor,
@@ -89,7 +119,7 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
               ),
             ),
             Flexible(
-              flex: 1,
+              flex: 0,
               child: Form(
                 key: _formKey,
                 child: CustomTextFormField(
@@ -117,18 +147,29 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
             ),
             Flexible(
               flex: 1,
-              child: ElevatedButton(
-                onPressed: _email == null ? null : () => _submit(context),
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 0.1.wp,
-                    vertical: 0.01.hp,
-                  ),
-                  child: AutoSizeText(
-                    'Confirmar',
-                    style: GoogleFonts.montserrat(
-                        fontSize: 36.nsp, color: Colors.white),
-                  ),
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                width: 0.4.wp,
+                child: ElevatedButton(
+                  onPressed: _email == null || _loading
+                      ? null
+                      : () => _submit(context),
+                  child: _loading
+                      ? SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                            strokeWidth: 2,
+                          ),
+                        )
+                      : AutoSizeText(
+                          'Confirmar',
+                          style: GoogleFonts.montserrat(
+                            fontSize: 36.nsp,
+                            color: Colors.white,
+                          ),
+                        ),
                 ),
               ),
             ),
