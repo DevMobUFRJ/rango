73c9c94f732e902291f1356441c782ef24cf63dd
Guilherme Franco <guilherme.franco@moneri.com.br>
2021-07-28 20:50:53 -0300

    Remove chave das constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,3 @@
-final firabaseMessagingAuthorizationKey =
-    'AAAA1KntTNc:APA91bHKXFtTYr5KpXeodW9uNneNZd2D6wK20FxE2gSgCL9ujlUfG-mx8tZp2RceOEKZxvLkRyBV-wsY1Fg8U8W28S8G_7KeYgY02ytvPIn9V_jERUUupQfGZrsEDQcdEaTT6V6kvOGL';
 final noInternetMessage =
     'Você está sem conexão de internet. Clique abaixo para tentar novamente';
 final defaultErrorMessage = 'Ocorreu um erro, tente novamente';
